4788a97467be581cd53a97fcdf4b3a5714eeb508
drummdurum <147395124+drummdurum@users.noreply.github.com>
2024-09-12 13:48:50 +0200

    jeg prøver igen

---
+++ src/main/java/com/example/testcdigen/TestCdIgenApplication.java
@@ -0,0 +1,13 @@
+package com.example.testcdigen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestCdIgenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestCdIgenApplication.class, args);
+    }
+
+}
